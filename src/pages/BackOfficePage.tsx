d6ed4e56858044614c8c276b7ffe0e9e7bf7f67d
Vitaly <124804105+Vitac86@users.noreply.github.com>
2026-04-21 23:45:35 +0300

    Добавить страницы отчетов для middle/back/depository

---
+++ src/pages/BackOfficePage.tsx
@@ -0,0 +1,5 @@
+import { ReportsPageTemplate } from '../components/crm/ReportsPageTemplate';
+
+export const BackOfficePage = () => {
+  return <ReportsPageTemplate title="Отчёты Бэк-офиса" department="Бэк-офис" />;
+};
---
+++ src/pages/DepositoryPage.tsx
@@ -0,0 +1,5 @@
+import { ReportsPageTemplate } from '../components/crm/ReportsPageTemplate';
+
+export const DepositoryPage = () => {
+  return <ReportsPageTemplate title="Отчёты депозитария" department="Депозитарий" />;
+};
